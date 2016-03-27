c690de25735ed6e496f2036f6329affff35979f3
宋飞 <isongfei@gmail.com>
2016-03-27 11:58:22 +0800

    Initial Commit

---
+++ Trendings-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trendings-iOS
+//
+//  Created by SongFei on 16/3/27.
+//  Copyright © 2016年 SongFei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
