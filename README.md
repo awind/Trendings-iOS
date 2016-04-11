6ee2b03685412e7a7f9b2d649a285c64406adef3
songfei <isongfei@gmail.com>
2016-04-11 20:53:11 +0800

    add README

---
+++ README.md
@@ -0,0 +1,57 @@
+<img src="Screenshot/appIcon.png" width="110" align="center">
+# Trending
+##### *View the most trending repositories and developers in Github.*
+
+
+
+##Screenshots
+<br/>
+<img src="Screenshot/screenshot_1.png" width="210">
+<img src="Screenshot/screenshot_2.png" width="210">
+<img src="Screenshot/screenshot_3.png" width="210">
+<br/>
+<br/>
+
+-------
+
+# Third-party libraries used
+
++ [Kingfisher](https://github.com/onevcat/Kingfisher)
+
++ [RxSwift](https://github.com/ReactiveX/RxSwift)
+
++ [ActionSheetPicker-3.0](https://github.com/skywinder/ActionSheetPicker-3.0)
+
++ [Alamofire](https://github.com/Alamofire/Alamofire)
+
++ [Argo](https://github.com/thoughtbot/Argo)
+
++ [Curry](https://github.com/thoughtbot/Curry)
+
++ [Moya](https://github.com/Moya/Moya)
+
++ [MJRefresh](https://github.com/CoderMJLee/MJRefresh)
+
+
+# Contact 
+Follow and contact me on [Twitter](https://twitter.com/debitosou) or [Sina Weibo](http://www.weibo.com/isongfei). If you find an issue, just open a ticket on it. Pull requests are warmly welcome as well.
+
+# License
+
+
+ 	Copyright (c) 2016 Phillip Song 
+ 	
+ 	Licensed under the Apache License, Version 2.0 (the "License");
+ 	you may not use this file except in compliance with the License.
+ 	You may obtain a copy of the License at
+ 	
+ 	http://www.apache.org/licenses/LICENSE-2.0
+ 	
+ 	Unless required by applicable law or agreed to in writing, software
+ 	distributed under the License is distributed on an "AS IS" BASIS,
+ 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ 	See the License for the specific language governing permissions and
+ 	limitations under the License.
+ 
+
+
---
+++ Trendings-iOS/Networking/TrendingAPI.swift
@@ -0,0 +1,100 @@
+//
+//  File.swift
+//  Trendings-iOS
+//
+//  Created by SongFei on 16/3/27.
+//  Copyright © 2016年 SongFei. All rights reserved.
+//
+
+import Foundation
+import Moya
+import RxSwift
+import Alamofire
+
+
+private let provider = RxMoyaProvider<TrendingAPI>()
+private var disposeBag = DisposeBag()
+
+
+public enum TrendingAPI {
+    case Trending(String, String)
+    case Developer(String, String)
+    case Support
+}
+
+
+extension TrendingAPI: TargetType {
+    public var baseURL: NSURL { return NSURL(string : "http://162.243.45.82")! }
+    public var path: String {
+        switch self {
+        case .Trending(_, _):
+            return "/v2/trendings"
+        case .Developer(_, _):
+            return "/v2/developers"
+        case .Support:
+            return "/v2/support"
+        }
+    }
+    
+    public var method: Moya.Method {
+        return .GET
+    }
+    
+    public var parameters: [String: AnyObject]? {
+        switch self {
+        case .Trending(let language, let since):
+            return ["language": language, "since": since]
+        case .Developer(let language, let since):
+            return ["language": language, "since": since]
+        default:
+            return nil
+        }
+    }
+    
+    public var sampleData: NSData {
+        switch self {
+        default:
+            return NSData()
+        }
+        
+    }
+}
+
+extension TrendingAPI {
+    static func getTrendings(language: String, since: String, completion: RepoItems -> Void) {
+        //disposeBag = DisposeBag()
+        provider.request(.Trending(language, since))
+            .mapSuccessfulHTTPToObject(RepoItems)
+            .observeOn(MainScheduler.instance)
+            .subscribe(
+                onNext: { items in
+                    completion(items)
+                }
+            )
+            .addDisposableTo(disposeBag)
+    }
+    
+    static func getDevelopers(language: String, since: String, completion: DevItems -> Void) {
+        //disposeBag = DisposeBag()
+        provider.request(.Developer(language, since))
+            .mapSuccessfulHTTPToObject(DevItems)
+            .observeOn(MainScheduler.instance)
+            .subscribe(
+                onNext: { items in
+                    completion(items)
+                }
+            )
+            .addDisposableTo(disposeBag)
+    }
+}
+
+
+private extension String {
+    var URLEscapedString: String {
+        return self.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())!
+    }
+}
+
+public func url(route: TargetType) -> String {
+    return route.baseURL.URLByAppendingPathComponent(route.path).absoluteString
+}
